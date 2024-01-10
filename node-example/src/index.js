68ad340b8a9e8e55d11d5b54fcd6c85f0be04c5a
LucasScott2000 <scott-l25@ulster.ac.uk>
2024-01-10 23:53:19 +0000

    updated login screen

--- node-example/src/index.js
+++ node-example/src/index.js
@@ -1,12 +1,21 @@
-const express = require('express')
-const app = express()
+
+
 let nunjucks = require('nunjucks')
 const port = 3000
 const path = require('path')
-const bodyParser = require("body-parser");
+const express = require("express"),
+    mongoose = require("mongoose"),
+    passport = require("passport"),
+    bodyParser = require("body-parser"),
+    LocalStrategy = require("passport-local"),
+    passportLocalMongoose = 
+        require("passport-local-mongoose")
+const app = express()
+const User = require("../model/User");
 
 app.use(express.json())
 app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
 
 nunjucks.configure(['views/'], {
   autoescape: true,
@@ -16,7 +25,7 @@ nunjucks.configure(['views/'], {
 /////////Setting up Mongo//////////
 async function mongoConnect(){
   const { MongoClient, ServerApiVersion } = require('mongodb');
-  const uri = "mongodb+srv://<username>:<password>@cluster0.v5chlrq.mongodb.net/?retryWrites=true&w=majority";
+  const uri = "mongodb+srv://lscott:mongodb123@clusterghms.mqurwz8.mongodb.net/?retryWrites=true&w=majority";
 
   const client = new MongoClient(uri, {
     serverApi: {
@@ -34,11 +43,11 @@ async function mongoConnect(){
 ////////Express Application/////////
 app.get('/', async (req, res) => {
   connection = await mongoConnect();
-  db = connection.db("example-database");
-  collection = db.collection('example-collection');
+  db = connection.db("GHMS");
+  collection = db.collection('Users');
   documents = await collection.find({}).toArray();
   response_json = JSON.stringify(documents)
-
+  console.log(documents);
   res.render('index.ejs', {users: documents});
 })
 
@@ -51,11 +60,40 @@ app.get('/', async (req, res) => {
 //sendFile function to send a static HTML page 
 app.get('/home', async (req, res) => {
   res.sendFile(path.join(__dirname, '/index.html'));
+
+
 })
 
 app.get('/login', async (req, res) => {
-  res.sendFile(path.join(__dirname, '/login.html'));
+  res.render('login.ejs');
 })
+
+app.post('/login', async function(req, res){
+  try {
+      // check if the user exists
+      connection = await mongoConnect();
+      db = connection.db("GHMS");
+      collection = db.collection('Users');
+      documents = await collection.find({}).toArray();
+      response_json = JSON.stringify(documents);
+      
+      
+
+      const user = await collection.findOne({ username: req.body.username });
+    
+      //console.log(user);
+      if (user) {
+        //check if password matches
+        if (req.body.password === user.password) {
+          return res.render("secret");
+        } 
+      }
+      res.render('login.ejs',{ error: "Username or password is incorrect" });
+    
+    } catch (error) {
+      res.status(400).json({ error });
+    }
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
