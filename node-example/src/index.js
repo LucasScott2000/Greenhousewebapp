b7a7aa1be44c486d3447ca693cfab15fdc9c3d61
LucasScott2000 <scott-l25@ulster.ac.uk>
2024-05-07 23:43:16 +0100

    commented code

--- node-example/src/index.js
+++ node-example/src/index.js
@@ -11,9 +11,9 @@ const bcrypt = require('bcrypt');
 
 // Set up Express application
 const app = express();
-app.use(express.json());
-app.set('view engine', 'ejs');
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json()); // Middleware to parse JSON data
+app.set('view engine', 'ejs'); // Setting up EJS as the view engine
+app.use(bodyParser.urlencoded({ extended: true })); // Middleware to parse URL-encoded data
 
 // Configure Nunjucks
 nunjucks.configure(['views/'], {
@@ -34,10 +34,11 @@ async function mongoConnect() {
     }
   });
 
-  return await client.connect();
+  return await client.connect(); // Connecting to the MongoDB database
 }
 
 // Express routes
+// Route to render the homepage
 app.get('/', async (req, res) => {
   res.render('greenhouse.ejs');
 });
@@ -52,37 +53,35 @@ app.get('/login', async (req, res) => {
   res.render('login.ejs');
 });
 
-// Login form submission
+// Login form submission route
 app.post('/login', async function(req, res){
   try {
+    // Connecting to MongoDB
     connection = await mongoConnect();
     db = connection.db("GHMS");
     users = db.collection('Users');
     documents = await users.find({}).toArray();
     response_json = JSON.stringify(documents);
-  
-    // const saltRounds = 10;
-    // const salt = bcrypt.genSaltSync(saltRounds);
-    
-    // // Hash a password
-    // const plainPassword = req.body.password;
-    // const hashedPassword = bcrypt.hashSync(plainPassword, salt);
-    // console.log(hashedPassword)
 
+    // Checking username and password
     const user = await users.findOne({ username: req.body.username });
-  
+
+    // Comparing hashed passwords
     if (user && (await bcrypt.compare(req.body.password, user.password))) {
       return res.redirect('/menu?user=' + encodeURIComponent(user.username));
     }
+    // Rendering login page with error message if authentication fails
     res.render('login.ejs',{ error: "Username or password is incorrect" });
-  
+
   } catch (error) {
+    // Handling errors
     res.status(400).json({ error });
   }
 });
 
 // Route for historical data
 app.get('/historical', async (req, res) => {
+  // Connecting to MongoDB and retrieving historical sensor data
   connection = await mongoConnect();
   db = connection.db("GHMS");
   histsensors = db.collection('History');
@@ -91,18 +90,9 @@ app.get('/historical', async (req, res) => {
   return res.render("historical.ejs", {histsensors:documents});  
 });
 
-// Render login page with sensor data
-app.get('/login', async (req, res) => {
-  connection = await mongoConnect();
-  db = connection.db("GHMS");
-  sensors = db.collection('Sensors');
-  documents = await sensors.find({}).toArray();
-  response_json = JSON.stringify(documents);
-  return res.render("login.ejs", {sensors:documents}); 
-});
-
 // Route for environmental selection
 app.get('/envselect', async (req, res) => {
+  // Connecting to MongoDB and retrieving sensor data for environmental selection
   connection = await mongoConnect();
   db = connection.db("GHMS");
   sensors = db.collection('Sensors');
@@ -111,19 +101,9 @@ app.get('/envselect', async (req, res) => {
   return res.render("envselect.ejs", {sensors:documents}); 
 });
 
-// Route for user's greenhouse
-//app.get('/yourgreenhouse', async (req, res) => {
- // connection = await mongoConnect();
- // db = connection.db("GHMS");
- // sensors = db.collection('Sensors');
- // documents = await sensors.find({}).toArray();
- // response_json = JSON.stringify(documents);
- // console.log(documents)
- // return res.render("yourgreenhouse.ejs", {sensors:documents}); 
-//});
-
 // Route for changing greenhouse
 app.get('/changegreenhouse', async (req, res) => {
+  // Connecting to MongoDB and retrieving sensor data for changing greenhouse settings
   connection = await mongoConnect();
   db = connection.db("GHMS");
   sensors = db.collection('Sensors');
@@ -134,6 +114,7 @@ app.get('/changegreenhouse', async (req, res) => {
 
 // Route for greenhouse data
 app.get('/greenhouse', async (req, res) => {
+  // Connecting to MongoDB and retrieving sensor and actuator data for greenhouse monitoring
   connection = await mongoConnect();
   db = connection.db("GHMS");
   sensors = db.collection('Sensors');
@@ -141,11 +122,12 @@ app.get('/greenhouse', async (req, res) => {
   actuators = db.collection('Actuators');
   documents2 = await actuators.find({}).toArray();
   response_json = JSON.stringify(documents);
-  return res.render("greenhouse.ejs", {sensors:documents, actuators:documents2}); // *change to login.ejs as this is the first page!*
+  return res.render("greenhouse.ejs", {sensors:documents, actuators:documents2});
 });
 
 // Route to get sensor data
 app.get('/getsensordata', async (req, res) => {
+  // Connecting to MongoDB and retrieving sensor data
   connection = await mongoConnect();
   db = connection.db("GHMS");
   sensors = db.collection('Sensors');
@@ -157,6 +139,7 @@ app.get('/getsensordata', async (req, res) => {
 // Route to get actuator states
 app.get('/getactuatorstates', async (req, res) => {
   try {
+    // Connecting to MongoDB and retrieving actuator states
     connection = await mongoConnect();
     db = connection.db("GHMS");
     actuators = db.collection('Actuators');
@@ -164,13 +147,15 @@ app.get('/getactuatorstates', async (req, res) => {
     res.setHeader('Content-Type', 'application/json');
     return res.json(documents);
   } catch (error) {
-    // If an error occurs, send an error response
+    // Handling errors
     console.error("Error retrieving actuator states:", error);
     return res.status(500).json({ error: "Internal Server Error" });
   }
 });
 
+// Route to get plant profile
 app.get('/getplantprofile', async (req, res) => {
+  // Connecting to MongoDB and retrieving plant profile data
   connection = await mongoConnect();
   db = connection.db("GHMS");
   profile = db.collection('PlantProfiles');
@@ -179,7 +164,9 @@ app.get('/getplantprofile', async (req, res) => {
   return res.json(documents);
 });
 
+// Route to get actuators
 app.get('/getactuators', async (req, res) => {
+  // Connecting to MongoDB and retrieving actuator data
   connection = await mongoConnect();
   db = connection.db("GHMS");
   profile = db.collection('Actuators');
@@ -189,58 +176,29 @@ app.get('/getactuators', async (req, res) => {
 });
 
 // Route to update actuators
-// Route to update actuators
-// Route to update actuators
 const { MongoClient, ObjectId } = require('mongodb');
 
 // Update actuators route - THIS CODE UPDATES THE ACTUATORS
-// app.post('/updateactuators', async (req, res) => {
-//   // Assuming req.body contains the data to update
-//   const newData = req.body;
-
-//   try {
-//     // Connect to the MongoDB database
-//     const connection = await mongoConnect();
-//     const db = connection.db("GHMS");
-//     const profile = db.collection('Actuators');
-
-//     // Update the data in the database
-//     await profile.updateMany({}, { $set: newData });
-
-//     // Fetch the updated data from the database
-//     const updatedData = await profile.find({}).toArray();
-
-//     // Respond with the updated data
-//     res.setHeader('Content-Type', 'application/json');
-//     return res.json(updatedData);
-//   } catch (error) {
-//     // Handle errors
-//     console.error("Error updating data:", error);
-//     res.status(500).json({ error: "Error updating data" });
-//   }
-// });
-
 app.post('/updateactuator/:actuatorName', async (req, res) => {
   try {
     const { actuatorName } = req.params;
     const newData = req.body;
 
-    // Connect to the MongoDB database
+    // Connecting to MongoDB and updating specific actuator's data
     const connection = await mongoConnect();
     const db = connection.db("GHMS");
     const profile = db.collection('Actuators');
 
-    // Update the specific actuator's data in the database
     await profile.updateOne({ actuator_name: actuatorName }, { $set: newData });
 
-    // Fetch the updated data from the database
+    // Fetching the updated data from the database
     const updatedData = await profile.findOne({ actuator_name: actuatorName });
 
-    // Respond with the updated data
+    // Responding with the updated data
     res.setHeader('Content-Type', 'application/json');
     return res.json(updatedData);
   } catch (error) {
-    // Handle errors
+    // Handling errors
     console.error("Error updating data:", error);
     res.status(500).json({ error: "Error updating data" });
   }
@@ -248,17 +206,20 @@ app.post('/updateactuator/:actuatorName', async (req, res) => {
 
 // Route for menu
 app.get('/menu',(req, res) => {
+  // Retrieving username from query parameters and rendering the menu page
   let user = req.query.user;
   return res.render("menu.ejs", {user});
 });
 
 // Route for Settings
 app.get('/settings', (req, res) => {
+  // Rendering the settings page
   res.render('settings');
 });
 
 // Start server
 const port = 3000;
 app.listen(port, () => {
+  // Starting the server and listening on specified port
   console.log(`Example app listening on port ${port}`);
 });
